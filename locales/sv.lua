be9e13fe2b17aa2384fb23aada7b4a8f889844c4
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-08-02 22:15:53 +0200

    Added more editable text

--- locales/sv.lua
+++ locales/sv.lua
@@ -1,4 +1,35 @@
 Locales['sv'] = {
   -- Marker Hint
-  	['marker_hint'] = 'Klicka ~INPUT_CONTEXT~ för att titta på ~g~Kameraövervakningen~s~',
+  	['marker_hint'] = 'Klicka ~INPUT_CONTEXT~ för att titta på ~g~kamerorna~s~',
+	
+	-- Meny
+	['securitycams_menu'] = 'Säkerhetskameror',
+	['bank_menu_selection'] = 'Pacific Standard Bank',
+	['police_menu_selection'] = 'Polisstation',
+	
+	-- I Kamerorna
+	['pacific_standard_bank'] = 'Pacific Standard Bank',
+	['police_station'] = 'Polisstation',
+	
+	-- Pacific Standard Bank Kameror
+	['bcam'] = 'Entré',
+	['bcam2'] = 'Lobby',
+	['bcam3'] = 'Mindre Entré',
+	['bcam4'] = 'Trappa Till Andra Våningen',
+	['bcam5'] = 'Trappa Ovanför Bankvalvet',
+	['bcam6'] = 'Kontorens Korridor #1',
+	['bcam7'] = 'Kontorens Korridor #2',
+	['bcam8'] = 'Andra Våningen #1',
+	['bcam9'] = 'Andra Våningen #2',
+	['bcam10'] = 'Trappa Till Bankvalvet',
+	['bcam11'] = 'Utanför Bankvalvet',
+	
+	-- Polisstationens Kameror
+	['pcam'] = 'Parkering',
+	['pcam2'] = 'Cell #1',
+	['pcam3'] = 'Cell #2',
+	['pcam4'] = 'Cell #3',
+	['pcam5'] = 'Polisens Parkering & Garage',
+	['pcam6'] = 'Utanför Entré',
+	['pcam7'] = 'Lobby',
 }
