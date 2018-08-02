e376db3798b2dacee93931c364d6683a6cfc24f7
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-08-02 22:16:13 +0200

    Added more editable text

--- locales/en.lua
+++ locales/en.lua
@@ -1,4 +1,35 @@
 Locales['en'] = {
   -- Marker Hint
-  	['marker_hint'] = 'Press ~INPUT_CONTEXT~ to connect to the ~g~Cameras~s~',
+  	['marker_hint'] = 'Press ~INPUT_CONTEXT~ to connect to the ~g~cameras~s~',
+	
+	-- Menu
+	['securitycams_menu'] = 'Security Cameras',
+	['bank_menu_selection'] = 'Pacific Standard Bank',
+	['police_menu_selection'] = 'Police Station',
+	
+	-- In Cameras
+	['pacific_standard_bank'] = 'Pacific Standard Bank',
+	['police_station'] = 'Police Station',
+	
+	-- Pacific Standard Bank Cameras
+	['bcam'] = 'Main Entrance',
+	['bcam2'] = 'Lobby',
+	['bcam3'] = 'Second Entrance',
+	['bcam4'] = 'Staircase To Second Floor',
+	['bcam5'] = 'Staircase Above Bankvault',
+	['bcam6'] = 'Offices Corridor #1',
+	['bcam7'] = 'Offices Corridor #2',
+	['bcam8'] = 'Second Floor #1',
+	['bcam9'] = 'Second Floor #2',
+	['bcam10'] = 'Staircase To Bankvault',
+	['bcam11'] = 'Outside The Bankvault',
+	
+	-- Police Station Cameras
+	['pcam'] = 'Parking Lot',
+	['pcam2'] = 'Cell #1',
+	['pcam3'] = 'Cell #2',
+	['pcam4'] = 'Cell #3',
+	['pcam5'] = 'Polices Parking Lot & Garage',
+	['pcam6'] = 'Outside Entrance',
+	['pcam7'] = 'Lobby',
 }
